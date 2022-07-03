1d1438624a58458d4b38714b3f7a88c8a082027d
josephdsbr@gmail.com <josephdsbr@gmail.com>
2022-07-03 18:22:05 -0300

    finishing footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:5581997667754">+55 (81) 9 9766-7754</LinkItem>
+        </LinkColumn>
+
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:josephdsbr@gmail.com">josephdsbr@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/josephdsbr">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+
+          <SocialIcons href="https://www.linkedin.com/in/josephdsbr/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+
+          <SocialIcons href="https://github.com/">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
